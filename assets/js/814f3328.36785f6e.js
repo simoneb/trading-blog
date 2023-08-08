590ab94254369dff1ba717b06723ef6bb19f0e1b
simoneb <simoneb@users.noreply.github.com>
2023-08-08 22:23:10 +0000

    deploy: 14f83cbed65862c22cde80f5fa5173f75e59f6ff

--- assets/js/814f3328.36785f6e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktrading_blog=self.webpackChunktrading_blog||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"The systematic trading journey of a professional software developer","permalink":"/trading-blog/the-systematic-trading-journey-of-a-professional-software-developer"}]}')}}]);
---
+++ assets/js/814f3328.a1c412ec.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktrading_blog=self.webpackChunktrading_blog||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"A software developer\'s trading journey","permalink":"/trading-blog/a-software-developers-trading-journey"}]}')}}]);
