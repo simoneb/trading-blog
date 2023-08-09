96a520a04fd171649a1b5286ac3887e787de5195
Simone Busoli <simone.busoli@gmail.com>
2023-08-09 16:22:29 +0200

    feat: add github icon and algolia search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,13 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'The Trading Developer',
-  tagline: 'A software developer\'s journey into equity trading',
+  tagline: "A software developer's journey into equity trading",
   favicon: 'img/trading-developer-logo.png',
   url: 'https://simoneb.github.io',
   baseUrl: '/trading-blog',
@@ -51,10 +51,40 @@ const config = {
         items: [
           {
             href: 'https://github.com/simoneb',
-            label: 'GitHub',
             position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub',
           },
         ],
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'BT6JBA6IFH',
+
+        // Public API key: it is safe to commit it
+        apiKey: '41f7c3498f5e68c9316d6a25d64069a6',
+
+        indexName: 'trading',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        //... other Algolia params
       },
       footer: {
         style: 'dark',
@@ -90,13 +120,13 @@ const config = {
           //   ],
           // },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Simone Busoli. Built with <a target="_blank" href="https://docusaurus.io/">Docusaurus</a>.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Simone Busoli. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
     }),
-};
+}
 
-module.exports = config;
+module.exports = config
